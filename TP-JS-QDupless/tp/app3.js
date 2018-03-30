4eec69f29922236a64fc0a728d83a8492fcaa0f2
Quentin <messirequentin@hotmail.fr>
2018-03-30 16:46:54 +0200

    Delete app3.js

--- TP-JS-QDupless/tp/app3.js
+++
@@ -1,9 +0,0 @@
-var block = document.getElementsByTagName('div');
-console.log(block);
-
-var ft_fade = function(e)
-{
-	block[0].className = "square fade";
-}
-
-block[0].addEventListener('click', ft_fade);
