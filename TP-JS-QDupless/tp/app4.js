d13256dbe8ee91e4340b9d3eddfc09d91bbb8666
Quentin <messirequentin@hotmail.fr>
2018-03-30 16:47:06 +0200

    Delete app4.js

--- TP-JS-QDupless/tp/app4.js
+++
@@ -1,9 +0,0 @@
-var squares = document.querySelectorAll('.square');
-squares.forEach(function(e){
-	e.addEventListener('click', function(e){
-		var select = document.querySelectorAll('.selected');
-		if (select[0])
-			select[0].className = "square";
-		e.originalTarget.className = "square selected";
-	})
-})
